237f759c9aa670ec7b32cbbabac98ffcf3577cc4
Prem Mhatre <83350182+Prem-Mhatre@users.noreply.github.com>
2021-11-07 16:19:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 document.getElementById("openTb_button").addEventListener("click", () => {
-    if(document.getElementById("tabs").style.left == -80 + "px"){
-        document.getElementById("tabs").style.left = -0 + "px";
-        document.querySelector(".blur_background").style.background = "grey";
+    if(document.getElementById("tabs").style.left == -300 + "px"){
+        document.getElementById("tabs").style.left = 0 + "px";
+        setTimeout(function(){
+            document.querySelector(".blur_background").style.background = "grey";
+            document.querySelector(".blur_background").style.height = 100 + "vh";
+        },200)
     }
     else{
-        document.getElementById("tabs").style.left = -80 + "px";
+        document.getElementById("tabs").style.left = -300 + "px";
         document.querySelector(".blur_background").style.background = "transparent";
+        document.querySelector(".blur_background").style.height = 0;
     }
 });
+
+document.querySelector(".blur_background").addEventListener("click", () => {
+    if(document.getElementById("tabs").style.left == 0 + "px"){
+        document.getElementById("tabs").style.left = -300 + "px";
+        document.querySelector(".blur_background").style.background = "transparent";
+        document.querySelector(".blur_background").style.height = 0;
+    }
+})
+
+    function getData(){
+        checkSloganCollapsed();
+    }
+
+    getData();
+
+    function checkSloganCollapsed(){
+        if(window.innerWidth < 721){
+            console.log("slogan is collapsed");
+    
+            document.getElementById("slogan_tab").innerHTML = "#No Broker No Commission";
+        }
+    }
